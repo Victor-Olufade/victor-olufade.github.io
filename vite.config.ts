73972f94432096b258cc36a5bbbf53bafc5b1ce4
OlufadeV <OlufadeV@accessbankplc.com>
2025-01-28 19:02:18 +0100

    transpile dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   base: '/your-repo-name/', // Add your repository name here
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "victor-olufade.github.io",
+  build: {
+    outDir: "dist",
+  },
+});
